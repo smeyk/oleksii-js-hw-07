6f725a72d1680381800c7362c9668a68f1045475
Olexii Guberniuk <oleksii@MacBook-Pro-Olexii.local>
2023-10-22 16:11:38 +0200

    add task1 and task2

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,39 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const gallery = document.querySelector(".gallery");
 
-console.log(galleryItems);
+const galeryItem = galleryItems
+	.map((image) =>
+		`<li class="gallery__item">
+		<a class="gallery__link" href="${image.original}">
+		<img class="gallery__image" src="${image.preview}" alt="${image.description}" data-source="${image.original}" />
+		</a>
+		</li>`
+	)
+	.join("")
+gallery.insertAdjacentHTML("afterbegin", galeryItem);
+
+const instance = basicLightbox.create(`<img src="" />`);
+
+const showImage = (event) => {
+	event.preventDefault();
+	const datasetImgSrc = event.target.dataset.source;
+	instance.element().querySelector('img').src = datasetImgSrc;
+	instance.show();
+}
+
+const closeImg = (event) => {
+	if (event.key === "Escape" && !instance.close()) {
+		instance.close();
+		console.log("show");
+	}
+}
+
+gallery.addEventListener("click", showImage);
+document.addEventListener("keydown", closeImg);
+
+
+
+
+
+
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,22 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const gallery = document.querySelector(".gallery");
 
-console.log(galleryItems);
+const galeryItem = galleryItems
+	.map((image) =>
+		`<li class="gallery__item">
+		<a class="gallery__link" href="${image.original}">
+		<img class="gallery__image" src="${image.preview}" alt="${image.description}" />
+		</a>
+		</li>`
+	)
+	.join("")
+gallery.insertAdjacentHTML("afterbegin", galeryItem);
+
+
+const lightbox = new SimpleLightbox('.gallery__item .gallery__link', {
+	captionsData: "alt",
+	captionDelay: 250,
+});
+
+
